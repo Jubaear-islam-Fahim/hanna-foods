f1e854dcc5d1e93f3defdc97f4c0076080b293d8
Jubaear <jubaearislam86664@gmail.com>
2021-12-11 23:42:38 +0600

    new page add

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,9 +16,12 @@
             dots: false,
             infinite: false,
             speed: 300,
-            slidesToShow: 3,
-            slidesToScroll: 4,
-            responsive: [{
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            prevArrow: '<button class="bi bi-chevron-left slide-arrow prev-arrow"></button>',
+            nextArrow: '<button class="bi bi-chevron-right slide-arrow next-arrow"></button>',
+            responsive: [
+                {
                     breakpoint: 1024,
                     settings: {
                         slidesToShow: 3,
@@ -73,8 +76,40 @@
             dots: false,
             centerMode: true,
             focusOnSelect: true,
-            prevArrow: '<button class="slide-arrow prev-arrow"></button>',
-            nextArrow: '<button class="slide-arrow next-arrow"></button>'
+            prevArrow: '<button class="bi bi-chevron-left slide-arrow prev-arrow"></button>',
+            nextArrow: '<button class="bi bi-chevron-right slide-arrow next-arrow"></button>'
+        });
+    }
+
+    if ($('.team-slid').length) {
+        $('.team-slid').slick({
+            dots: true,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3, 
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         });
     }
 
